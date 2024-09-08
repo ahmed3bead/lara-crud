5ffb49af8db81327e35ac4b4c8378cba34e4b093
Ahmed Ebead <ahmed.ebead@munjz.com>
2024-09-08 18:34:19 +0300

    Add route inclusion to api.php if not already present

--- src/Console/Commands/CrudBlueprintApiControllerCommand.php
+++ src/Console/Commands/CrudBlueprintApiControllerCommand.php
@@ -301,7 +301,6 @@ EOD;
             $filePath = base_path('routes' . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $name . '.php');
         }
         $routeFilePath.=  $this->configs['base_route_file_name']. '.php';
-dd($routeFilePath);
         $this->createFile($filePath, $stub);
         $routeName = str_replace('_', '-', $name);
         $lineToAdd = "Route::prefix('{$routeName}')->group(function () {include_once 'modules/{$name}.php';});";
